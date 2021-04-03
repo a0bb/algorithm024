97aeceee1daaddf60940668b2fc4db4831a3b8b0
wangshihua <wangsh.a0bb@gmail.com>
2021-04-04 01:56:40 +0800

    addcode：week09 init

---
+++ Week_09/56. 合并区间.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+"""
+将列表中的区间按照左端点升序排序。然后我们将第一个区间加入 merged 数组中，并按顺序依次考虑之后的每个区间：
+- 如果当前区间的左端点在数组 merged 中最后一个区间的右端点之后，那么它们不会重合，我们可以直接将这个区间加入数组 merged 的末尾；
+- 否则，它们重合，我们需要用当前区间的右端点更新数组 merged 中最后一个区间的右端点，将其置为二者的较大值
+"""
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            # 如果列表为空，或者当前区间与上一区间不重合，直接添加
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                # 否则的话，我们就可以与上一区间进行合并
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
--- Week_09/README.md
+++ Week_09/README.md
@@ -1,1 +1,11 @@
 学习笔记
+
+总结如下：
+
+![avatar](extra/LRU Cache.png)
+
+![avatar](extra/布隆过滤器BloomFilter.png)
+
+![avatar](extra/排序算法.png)
+
+
